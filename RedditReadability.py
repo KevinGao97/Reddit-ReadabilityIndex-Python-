80d39a02b923d7717d823faee07925e187e12e1b
Zi Shuai Gao <keving.gao@mail.utoronto.ca>
2020-07-23 15:05:36 -0400

    Impored and finished implementing Coleman-Liau Index for top comments, including finding the average index score.

--- RedditReadability.py
+++ RedditReadability.py
@@ -9,7 +9,7 @@ and '[removed]' comments.
 def sanitizeComments(submission):
 
     allComments = []
-
+    
     for topComments in submission.comments:
         try:
             if topComments.body.find("[removed]") == -1:
@@ -18,13 +18,42 @@ def sanitizeComments(submission):
             print("Did not add the comment")
             continue
 
-    for i in range(len(allComments)):
-        print(allComments[i])
-        print("Coleman-Liau Index: " + str(colemanLiauIndex(allComments[i])))
-        print("\n =========================================== \n")
-    
     return allComments
 
+
+"""
+Finds the Coleman-Liau index for each comment in the topCommentList list. 
+Computes an average Coleman-Liau index of the thread using the topCommentList list
+
+"""
+def computeColemanLiauIndex(topCommentList):
+
+    avgColemanLiauIndex = []
+
+    for i in range(len(topCommentList)):
+        colemanLiauIndexScore = colemanLiauIndex(topCommentList[i])
+        avgColemanLiauIndex.append(colemanLiauIndexScore)
+        print(topCommentList[i])
+        if colemanLiauIndexScore < 1 :
+            print("Coleman-Liau Index: Preschool")
+        elif colemanLiauIndexScore >= 1 and colemanLiauIndexScore <= 12:
+            print("Coleman-Liau Index: Grade "+ str(colemanLiauIndexScore))
+        else:
+            print("Coleman-Liau Index: University level")
+
+        print("\n =========================================== \n")
+    
+    #Prints the average Coleman-Liau Index of the thread 
+    avgIndexScore = round(sum(avgColemanLiauIndex)/len(avgColemanLiauIndex))
+    if avgIndexScore < 1 :
+        print("Average Coleman-Liau Index: Preschool")
+    elif avgIndexScore >= 1 and avgIndexScore <= 12:
+        print("Average Coleman-Liau Index: Grade "+ str(avgIndexScore))
+    else:
+        print("Average Coleman-Liau Index: University level")
+
+
+    
 
 """
 The Main Function
@@ -34,14 +63,15 @@ def main():
 
     
     reddit = praw.Reddit(user_agent="Comment Extraction (by /u/USERNAME)",
-                        client_id="CLIENT_ID", client_secret="CLIENT_SECRET",
-                        username="USERNAME", password="PASSWORD")
+                     client_id="CLIENT_ID", client_secret="CLIENT_SECRET",
+                     username="USERNAME", password="PASSWORD")
 
     url = input("Please enter a reddit url: ")
     submission = reddit.submission(url=url)
 
+    topCommentList = sanitizeComments(submission)
+    computeColemanLiauIndex(topCommentList)
 
-    sanitizeComments(submission)
 
 main()
 
